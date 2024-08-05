8e942836cbfd55b027bcb5c9afdc4d3c6765f47e
ravvas <ravvas@rediffmail.com>
2024-08-05 16:29:09 +0530

    create go file

--- main.go
+++ main.go
@@ -2,29 +2,6 @@ package main
 
 import "fmt"
 
-type Article struct {
-	Title  string
-	Author string
-}
-
-func (a Article) String() string {
-	return fmt.Sprintf("The %q article was written by %s.", a.Title, a.Author)
-}
-
-type Stringer interface {
-	String() string
-}
-
 func main() {
-	a := Article{
-		Title:  "Understanding Interfaces in Go",
-		Author: "Sammy Shark",
-	}
-
-	Print(a)
-	// fmt.Println(a.String())
-}
-
-func Print(a Article) {
-	fmt.Println(a.String())
+	fmt.Println("Hello, World!")
 }
